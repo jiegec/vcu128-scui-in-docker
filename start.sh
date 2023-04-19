194eb7b484d6dd25a3c360d3a8346e519b764031
Jiajie Chen <c@jia.je>
2023-04-19 20:01:33 +0800

    Use --device to pass ttyUSB

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it --rm --network=host -v /dev/ttyUSB3:/dev/ttyUSB3 -v $HOME:$HOME -v /opt:/opt -e DISPLAY=$DISPLAY -e XAUTHORITY=$HOME/.Xauthority vcu128-scui-docker sh -c "cd $PWD/vcu128_scui/BoardUI && ./BoardUI"
+sudo docker run -it --rm --network=host --device=/dev/ttyUSB1 --device=/dev/ttyUSB2 --device=/dev/ttyUSB3 -v $HOME:$HOME -v /opt:/opt -e DISPLAY=$DISPLAY -e XAUTHORITY=$HOME/.Xauthority vcu128-scui-docker sh -c "cd $PWD/vcu128_scui/BoardUI && ./BoardUI"
